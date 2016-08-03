47f822dfbda0b3bdd19ffb0f481fafbf2c4ae5b3
DewmiR <dewmirandika@gmail.com>
2016-08-03 02:47:33 -0700

    server

---
+++ app.js
@@ -0,0 +1,139 @@
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+app.use(bodyParser.json());
+
+Genres = require('./models/genre');
+Books = require('./models/book');
+
+mongoose.connect('mongodb://localhost/bookstore');
+var db = mongoose.connection;
+
+
+app.get('/', function (req, res) {
+	 res.send('Hello World');
+});
+
+
+app.get('/api/genres', function (req, res) {
+	 Genres.getGenres(function (err,genres) {
+		 if(err){
+			 throw err;
+		 }
+		 res.json(genres);
+	 });
+});
+
+app.post('/api/genres', function (req, res) {
+
+	var genre= req.body;
+
+	Genres.addGenres(genre,function (err,genres) {
+		if(err){
+			throw err;
+		}
+		res.json(genres);
+	});
+});
+
+
+app.put('/api/genres/:id', function (req, res) {
+
+	var id = req.params.id;
+	var genre= req.body;
+
+	Genres.updateGenres(id,genre,{},function (err,genres) {
+		if(err){
+			throw err;
+		}
+		res.json(genres);
+	});
+});
+
+app.delete('/api/genres/:_id', function (req,res) {
+
+	var id = req.params._id;
+
+	Genres.removeGenres(id,function(err,genre){
+
+		if(err){
+			throw err;
+		}
+		res.json(genre);
+
+	});
+
+});
+
+
+app.get('/api/books', function (req,res) {
+	Books.getBooks(function (err,books) {
+		if(err){
+			throw  err;
+		}
+		res.json(books);
+	});
+	
+});
+
+app.get('/api/books/:_id', function (req,res) {
+      Books.getBookByID(req.params._id, function (err,book) {
+		 if(err){
+			 throw  err;
+		 }
+		  res.json(book);
+	  });
+
+});
+
+
+app.post('/api/book/', function (req,res) {
+
+	var book = req.body;
+
+	Books.addBook(book, function (err,book) {
+
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+
+});
+
+
+app.put('/api/book/:id', function (req, res) {
+
+	var id = req.params.id;
+	var book= req.body;
+
+	Books.updateBook(id,book,{},function (err,book) {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.delete('/api/book/:_id', function (req,res) {
+
+	var id = req.params._id;
+
+	Books.removeBook(id,function(err,book){
+
+		if(err){
+			throw err;
+		}
+		res.json(book);
+
+	});
+
+});
+
+
+app.listen(3000);
+console.log("server running on port 3000");
+
---
+++ models/book.js
@@ -0,0 +1,98 @@
+var mongoose = require('mongoose');
+
+var bookSchema = mongoose.Schema({
+
+   title:{
+       type:String,
+       require:true
+   },
+
+    genre:{
+        type:String,
+        require:true
+    },
+
+    desciption:{
+        type:String,
+        require:true
+    },
+
+    author:{
+        type:String,
+        require:true
+    },
+
+    publisher:{
+        type:String
+
+    },
+
+    pages:{
+        type:String
+    },
+
+
+    image_url:{
+        type:String,
+        require:true
+    },
+
+    buy_url:{
+        type:String,
+        require:true
+    },
+    
+    created_at:{
+        type:Date,
+        default :Date.now()
+    }
+
+});
+
+
+
+var Book = module.exports = mongoose.model('Book',bookSchema);
+
+//Get Books
+module.exports.getBooks = function (callback,limit) {
+    Book.find(callback).limit(limit);
+};
+
+//Get Book by ID
+module.exports.getBookByID = function (id,callback) {
+    Book.findById(id,callback);
+};
+
+//Add Books
+module.exports.addBook= function (book,callback) {
+    Book.create(book,callback);
+};
+
+
+module.exports.updateBook = function (id,book,options,callback) {
+
+    var query = {_id : id};
+    var update = {
+        title : book.title,
+        genre : book.genre,
+        author : book.description,
+        description : book.title,
+        publisher : book.publisher,
+        pages : book.pages,
+        image_url : book.image_url,
+        buy_url : book.buy_url
+
+    };
+
+      Book.findOneAndUpdate(query,update,options,callback);
+};
+
+//Remove Book
+module.exports.removeBook = function (id,callback) {
+
+    var query = {_id : id};
+
+    Book.remove(query,callback);
+};
+
+
